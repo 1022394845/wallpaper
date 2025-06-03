cd20173cff669c37cf4d5f9259379ee635ffa406
1022394845 <1022394845@qq.com>
2025-06-03 19:12:08 +0800

    分类页面

---
+++ src/pages/category/index.vue
@@ -0,0 +1,15 @@
+<script setup></script>
+
+<template>
+  <view class="category wrapper">
+    <WallpaperItem v-for="(item, index) in 12" :key="index" />
+  </view>
+</template>
+
+<style lang="scss" scoped>
+.category {
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  gap: 15rpx;
+}
+</style>
---
+++ src/pages/user/index.vue
@@ -0,0 +1,7 @@
+<script setup></script>
+
+<template>
+  <view class="user wrapper">user</view>
+</template>
+
+<style lang="scss" scoped></style>
